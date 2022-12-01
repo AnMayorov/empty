07d8a773a2d570c70eb9cfceae6acb53ef3b6880
Анатолий Майоров <majoroff.anatoliy@mail.ru>
2022-12-01 12:27:55 +0300

    Добавил alert, console.log() и переменные в файл script.js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+alert("Домашка!");
+console.log("Ни куда от домашки!");
+
+let title = "Переменная";
+let screens = "Переменная";
+let screenPrice = "Переменная";
+let rollback = "Переменная";
+let fullPrice = "Переменная";
+let adaptive = "Переменная";
+
+console.log("");
