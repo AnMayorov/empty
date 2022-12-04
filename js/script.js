fe5cb9e73be0013a81ecb80265f6f8dc8edd616f
Анатолий Майоров <majoroff.anatoliy@mail.ru>
2022-12-04 07:59:53 +0300

    Создал новую ветку Lesson02

--- js/script.js
+++ js/script.js
@@ -1,6 +1,23 @@
-let title = "Переменная";
-let screens = "Переменная";
-let screenPrice = "Переменная";
-let rollback = "Переменная";
-let fullPrice = "Переменная";
-let adaptive = "Переменная";
+const title = "Lesson02";
+const screens = "Простые, Сложные, Переменные";
+const screenPrice =5000;
+const rollback = 50;
+const fullPrice = 300000;
+const adaptive = true;
+
+const layout = "Стоимость верстки экранов" + " " + screenPrice + " " + "рублей/ долларов/гривен/юани";
+const development = "Стоимость  разработки сайта" + " " + fullPrice + " " + "рублей/ долларов/гривен/юани";
+
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
+console.log(screens.length);
+console.log(screens.toLowerCase());
+console.log(screens.split(", "));
+
+console.log(layout);
+console.log(development);
+
+console.log(fullPrice * (rollback / 100));
+
