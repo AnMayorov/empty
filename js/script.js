f7decb0cb61f294096aff19ca648304efbc33dfb
Анатолий Майоров <majoroff.anatoliy@mail.ru>
2022-12-15 13:10:21 +0300

    Тренировка с учебником

--- js/script.js
+++ js/script.js
@@ -1,10 +1,53 @@
 "use strict";
 
-const rollback = 50;
+setTimeout(makeUpUser, 5000);
+function makeUpUser() {
+  alert ('Are you going to stare at this boring page forever?');
+};
+
+let price = 28.99;
+let discount = 10;
+let total = price - (price * (discount / 100));
+function freeShipping() {
+  if (total > 25) {
+    alert('Smile!');
+  };
+}
+freeShipping();
+
+let count = 10;
+while (count > 0) {
+  count = count -1;
+}
+console.log(count);
+
+let dog = {name: 'Rover', weight: '35'};
+if (dog.weight > 30) {
+  alert('WOOF! WOOF!');
+} else {
+  alert('woof! woof!');
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// rollBack = 50;
 /*--------------------------------------*/
 // const title = prompt(
 //   'Как называется ваш проект?', 
-//   'Lesson04'
+//   'Калькулятор вёрстки'
 // );
 // const screens = prompt(
 //   'Какие типы экранов нужно разработать?',
@@ -17,53 +60,82 @@ const rollback = 50;
 // const adaptive = confirm(
 //   'Нужен ли адаптив на сайте?'
 // );
-/*--------------------------------------*/
+// /*--------------------------------------*/
 // const service1 = prompt(
 //   'Какой дополнительный тип услуги нужен?',
 //   'service-1'
 // );
 // const servicePrice1 = +prompt('Сколько это будет стоить?');
-/*--------------------------------------*/
+// /*--------------------------------------*/
 // const service2 = prompt(
 //   'Какой дополнительный тип услуги нужен?',
 //   'service-2'
 // );
 // const servicePrice2 = +prompt('Сколько это будет стоить?');
-/*--------------------------------------*/
+// /*--------------------------------------*/
+// let allServicePrices, diffPercent;
 // const fullPrice = screenPrice + servicePrice1 + servicePrice2;
 // const layout = "Стоимость верстки экранов" + " " + screenPrice + " " + "рублей";
 // const development = "Стоимость  разработки сайта" + " " + fullPrice + " " + "рублей";
 // const percent = fullPrice * (rollback / 100);
-// const servicePercentPrice = Math.ceil(fullPrice - percent);
+/*--------------------------------------*/
+// let getAllServicePrices = function(servicePrice1, servicePrice2) {
+//   return servicePrice1 + servicePrice2;
+// };
+// allServicePrices = getAllServicePrices(servicePrice1, servicePrice2);
 
-// if (fullPrice > 30000) {
-//   console.log('Даем скидку в 10%');
-// } else if ( fullPrice >= 15000 && fullPrice < 30000) { 
-//   console.log('Даем скидку в 5%');
-// } else if (fullPrice >= 0 && fullPrice < 15000) {
-//   console.log('Скидка не предусмотрена');
-// } else {
-//   console.log('Что-то пошло не так');
-// }
-/*--------------------------------------*/ 
+
+
+// let getServicePercentPrices = function(fullPrice, percent) {
+//   return fullPrice - percent;
+// };
+// diffPercent = getServicePercentPrices(fullPrice, percent);
+// /*--------------------------------------*/
+// const servicePercentPrice = Math.ceil(fullPrice - percent) + diffPercent;
+
+// const showTypeof = function(variable) {
+//   console.log(variable, typeof variable);
+// };
+
+// const getRollbackMessage = function(price) {
+//   if (price > 30000) {
+//     return 'Даем скидку в 10%';
+//   } else if (price >= 15000) {
+//     return 'Даем скидку в 5%';
+//   } else if (price >= 0) {
+//     return 'Скидка не предусмотрена';
+//   } else {
+//     return 'Что-то пошло не так';
+//   }
+// };
+/*--------------------------------------*/
+// showTypeof(title);
+// showTypeof(fullPrice);
+// showTypeof(adaptive);
+
+// console.log(
+//   screens.toLowerCase().split(", ")
+// );
+// console.log(getRollbackMessage(fullPrice));
+// console.log(diffPercent);
+
 // console.log(typeof title);
 // console.log(typeof fullPrice);
 // console.log(typeof adaptive);
 
 // console.log(title);
-// console.log(screens);
+
 // console.log(screenPrice);
 // console.log(adaptive);
 
 // console.log(screens.length);
-// console.log(
-//   screens.toLowerCase().split(", ")
-// );
+
 
 // console.log(development);
 // console.log(layout);
 // console.log(percent);
 // console.log(servicePercentPrice);
+// console.log(allServicePrices);
 
 
 
