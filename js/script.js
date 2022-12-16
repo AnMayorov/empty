12e7aee0bbfc9e6db94110f452a508b677c876ab
Анатолий Майоров <majoroff.anatoliy@mail.ru>
2022-12-16 08:32:02 +0300

    Тренировка с выражениями

---
+++ js/script.js
@@ -0,0 +1,37 @@
+/*
+let age = 25;
+let name = 'Owen';
+
+if (age > 14) {
+  alert ('Взрослый')    // Если возраст больше 14 - вывод в окне: 'Взрослый' 
+} else {
+  alert ('Добро пожаловать' + name + '!');  // в противном случае
+}
+
+let, const - набор команд;
+age, name - переменные для хранения значений;
+25, 'Owen' - значения переменных;
+*/
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+let temp = 10;
+console.log((9 / 5) * temp +32);
+
+const color = 'orange';
+if (color == 'orange') {
+  console.log(true);
+} else if (color == 'pinc') {
+  console.log(false);
+}
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+let name = "Martha";
+let string = "you'we won!";
+console.log("Martha" + ", " + string);
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+let level = 5;
+let points = 30000;
+let bonus = 3300;
+console.log((level * points) + bonus);
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+let num1 = 1000;
+let num2 = '108';
+console.log(num1 + num2);
