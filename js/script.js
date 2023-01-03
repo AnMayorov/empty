0495b4b6cd9f5a227593c2147a0f06cec7d550c2
Анатолий Майоров <majoroff.anatoliy@mail.ru>
2023-01-03 09:32:39 +0300

    Добавил цикл for

--- js/script.js
+++ js/script.js
@@ -1,66 +1,27 @@
-// Пошаговое выполнение функции с командой (return):
-// ШАГ 1: объявляем переменную radius, присваиваем значение 5.2
-let radius = 5.2;
-// ШАГ 2: объявляем переменную theArea
-let theArea = calculateArea(radius); // ШАГ 3: присваиваем  theArea значение вызываемой функции, содержащей переменную radius как аргумент
-// ШАГ 4: пищем функцию calculateArea с параметром (r), которому передаётся аргумент radius:
-function calculateArea(r) {
-  let area; // ШАГ 5: с объявления переменной area начинается выполнение функции
-  if (r <= 0) { // Если r <= 0, то функция вернёт 0 и перестанет выполняться 
-    return 0;
-  } else { // ШАГ 6: выполняется else, т. к. r = 5.2 в нашем случае
-    // ШАГ 7: программа вычисляет площадь круга, используя значение 5.2 параметра r
-    area = Math.PI * r * r;
-    // ШАГ 8: функция возвращает вычисленное значение, которое сохранится в переменной theArea
-    return area;
-  }
-}
-// Вывод в консоль: 84.94866535306801
-console.log("The area is: " + theArea);
-// Функция без команды return вернёт undefined.
+// Перебор элементов массива с циклом while:
+let scores = [60, 50, 60, 58, 54, 54, 58, 50, 52, 54, 48, 69, 
+              34, 55, 51, 52, 44, 51, 69, 64, 66, 55, 52, 61,
+              46, 31, 57, 52, 44, 18, 41, 53, 55, 61, 51, 44];
 
-// расчёт фрактала:
-function clunk(times) {
-  let num = times;
-  while (num > 0) {
-    display("clunk");
-    num = num - 1;
-  }
-}
-function thingamajig(size) {
-  let facky = 1;
-  clunkCounter = 0;
-  if (size == 0) {
-    display("clunk");
-  } else if (size == 1) {
-    display("thunk");
-  } else {
-    while (size > 1) {
-      facky = facky * size;
-      size = size - 1;
-    }
-    clunk(facky);
-  }
-}
-function display(output) {
+// переменная для выводимой строки:              
+// let output;
+
+// переменная для хранения текущего индекса:
+// let i = 0;
+
+// пока индекс меньше длины массива, цикл продолжается:
+// while (i < scores.length) {
+  // выходная строка с номером раствора:
+  // output = "Bubble solution #" + i + " score: " + scores[i];
+  // console.log(output);
+
+  // индекс увеличивается на один перед повторным прохождением цикла:
+  // i = i + 1;
+//}
+
+// то же самое с циклом for:
+// инициализация -     условие -   увеличение:
+for (let i = 0; i < scores.length; i = i + 1) {
+  output = "Bubble solution #" + i + " score: " + scores[i];
   console.log(output);
-  clunkCounter = clunkCounter + 1;
 }
-let clunkCounter = 0;
-thingamajig(5);
-console.log(clunkCounter);
-
-let balance = 10500; // глобальная перемнная...
-let cameraOn = true;
-let amount = steal(balance, 1250); // но здесь она заменяется параметром 
-
-function steal(balance, amount) {
-  cameraOn = false;
-  if (amount < balance) {
-    balance = balance - amount; // попытка изменить переменную неудачна, т. к. она заменена параметром
-  }
-  return amount; // возврат украденной суммы, прекращение выполнения функции
-  cameraOn = true; // строки кода после return игнорируются
-}
-
-alert("Criminal: you stole " + amount + "!");
